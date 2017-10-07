a34b13e497c226ea24d78e1381a3cc5f264cca4d
Mario Duck <duckpradomario@gmail.com>
2017-10-06 17:50:41 -0700

    initial commit

---
+++ core/src/com/mygdx/game/States/GameStateManager.java
@@ -0,0 +1,39 @@
+package com.mygdx.game.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Stack;
+
+/**
+ * Created by Duck on 10/3/2017.
+ */
+
+public class GameStateManager {
+
+    private Stack<State> states;
+
+    public GameStateManager() {
+        states = new Stack<State>();
+    }
+
+    public void push(State state) {
+        states.push(state);
+    }
+
+    public void pop() {
+        states.pop();
+    }
+
+    public void state(State state) {
+        states.pop();
+        states.push(state);
+    }
+
+    public void update(float dt) {
+        states.peek().update(dt);
+    }
+
+    public void render(SpriteBatch sb) {
+        states.peek().render(sb);
+    }
+}
